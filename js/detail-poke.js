7d9ba25a03199b77efbdb9c39ca1fda89c848dd6
huynhthuan <huynhthuanvugia@gmail.com>
2021-04-29 09:07:28 +0700

    add quick btn in pokedex detail

--- js/detail-poke.js
+++ js/detail-poke.js
@@ -1,2 +1,12 @@
 let scene = document.getElementById('scene');
 let parallaxInstance = new Parallax(scene);
+let addBookMark = document.getElementById('addBookmark');
+
+addBookMark.onclick = () => {
+    Swal.fire({
+        title: 'Done!',
+        text: 'Add bookmark succesfull!',
+        icon: 'success',
+        confirmButtonText: 'Oki',
+    });
+};
